2ca57dcbf6c12e313bac58d31a40a194375e584a
chenjunhua <chenjunhua@qingzu.com.cn>
2019-09-19 15:13:23 +0800

    测试类冲突

--- src/main/java/com/cjh/douban/test/TestController2.java
+++ src/main/java/com/cjh/douban/test/TestController2.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
 @RefreshScope
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
