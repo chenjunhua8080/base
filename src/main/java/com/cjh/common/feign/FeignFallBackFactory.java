863d51484ad5f7171d235975e142e639b5d465bd
AllenChen <allen.chen@imanagesystems.com>
2021-04-27 20:24:51 +0800

    fix FallbackFactory

--- src/main/java/com/cjh/common/feign/FeignFallBackFactory.java
+++ src/main/java/com/cjh/common/feign/FeignFallBackFactory.java
@@ -1,6 +1,7 @@
 package com.cjh.common.feign;
 
 import feign.hystrix.FallbackFactory;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +11,16 @@ public class FeignFallBackFactory implements FallbackFactory<CloudFeignClient> {
 
     @Override
     public CloudFeignClient create(Throwable throwable) {
+        return new CloudFeignClient() {
+            @Override
+            public String pushErrorMsg(String openId, String body) {
+                return null;
+            }
 
-        log.error("in FeignFallBackFactory...");
-        throwable.printStackTrace();
-
-        return (openId, body) -> null;
+            @Override
+            public String pushResumeMsg(Map<String, Object> map) {
+                return null;
+            }
+        };
     }
 }
