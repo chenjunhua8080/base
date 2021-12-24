ba5ce310d6ba685db65cf5894f425d15411ddb5d
AllenChen <allen.chen@imanagesystems.com>
2021-12-24 14:14:20 +0800

    配置logback-spring.xml

--- src/main/java/com/cjh/common/CommonApplication.java
+++ src/main/java/com/cjh/common/CommonApplication.java
@@ -3,11 +3,12 @@ package com.cjh.common;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//加了第一次无法注册进去
-//@EnableDiscoveryClient
+//加了第一次无法注册进去?
+@EnableDiscoveryClient
 @EnableFeignClients
 @MapperScan("com.cjh.common.dao")
 public class CommonApplication {
